cfb94a380b6cdfac74d63efeb6f9d6f8c66e3732
Haley Parker <90306479@ep-student.org>
2022-02-23 11:07:16 -0600

    first commit

---
+++ storyboardTesting/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  storyboardTesting
+//
+//  Created by 90306479 on 2/17/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
--- storyboardTesting/ViewController.swift
+++ storyboardTesting/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var words:String = ""
+    
+    
+    @IBOutlet weak var wordsLabel: UILabel!
+    
+    @IBOutlet weak var moodField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
+    
+    
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        words = moodField.text!
 
+        wordsLabel.text = words
+        
+        if words.uppercased() == "HAPPY" {
+            view.backgroundColor = .yellow
+        }
+        if words.uppercased() == "SAD" {
+            view.backgroundColor = .blue
+        }
+        if words.uppercased() == "HUNGRY" {
+            view.backgroundColor = .red
+        }
+//
+//        let vc = SecondViewController()
+//        present(vc, animated: true)
+        
+    }
+    
 
 }
+
 
